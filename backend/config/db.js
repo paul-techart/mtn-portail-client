bf64293b2543400b90376d7c8bba2852cfefcb54
paul-techart <nkotopaularthur3@gmail.com>
2026-07-14 13:38:52 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,13 +5,14 @@ const mysql = require("mysql2/promise");
 // Si ton MySQL a un mot de passe, change juste DB_PASSWORD ci-dessous (ou via variable d'environnement).
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "mtn_portail_client",
   waitForConnections: true,
   connectionLimit: 10,
   namedPlaceholders: true,
-  ssl:{ rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
   connectTimeout: 30000,
 });
 
